c3fedc82738a8135b176eff90ae22331ab58caa1
dipika2204 <dipika.2204@csu.fullerton.edu>
2017-07-05 12:20:20 -0700

    Final Changes Done

--- server.js
+++ server.js
@@ -1,8 +1,4 @@
 var csv = require( "fast-csv" );
-
-
-// Require the 'request' module.
-// Install it with `npm install --save request`.
 var request = require('request');
 var express = require('express');
 var app = express();
@@ -14,7 +10,7 @@ app.use(function(req, res, next) {
     // Set permissive CORS header - this allows this server to be used only as
     // an API server in conjunction with something like webpack-dev-server.
     res.setHeader('Access-Control-Allow-Origin', '*');
-     res.setHeader('Access-Control-Allow-Headers' ,'Origin, X-Requested-With, Content-Type, Accept' );
+    res.setHeader('Access-Control-Allow-Headers' ,'Origin, X-Requested-With, Content-Type, Accept' );
     // Disable caching so we'll always get the latest comments.
     res.setHeader('Cache-Control', 'no-cache');
     next();
@@ -30,10 +26,9 @@ function MyCSV(Fcol,Scol,tcol,fcol,ficol,scol,secol,ecol,ncol){
   this.High=secol;
   this.Low=ecol;
   this.Mid=ncol;
-  
 }
 
-//var MyData={"Name":"dipika"};
+
 app.get('/currency_data', function(req, res) {
   var MyData=[];
   console.log("REquest ali");
@@ -44,34 +39,23 @@ request(URL, function (error, response, body) {
 var temp=response.body;
 console.log(temp);
 var inde=temp.split("\n");
-//console.log(inde.length);
-
-//MyData.push(inde);
 
 for(var index=0;index<inde.length;index++){
-//console.log(inde[index]);
+
 var temp1=inde[index].split(",");
 if(temp1[1]!=undefined){
 MyData.push({"Symbols":temp1[0],"Offer_big_figure":temp1[4],"Offer_big_points":temp1[5]});
-//console.log(MyData);
-}
-}
 
-//console.log(inde[10]
-  //MyData.push(new MyCSV())
-  //console.log(MyData[0]);
+}
+}
+//Converting the data into JSON Object
  var myJSON = JSON.stringify(MyData);
- //console.log(myJSON[0]);
   myJSON=JSON.parse(myJSON);
   console.log(myJSON);
   res.json(myJSON);
-
   }
 });
-
-
-  //console.log();
-  });
+});
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React,{Component} from 'react';
 import logo from './money-exchange.jpg';
-//import request from 'request';
 import './App.css';
 import Fadown from 'react-icons/lib/fa/hand-o-down';
 import Faup from 'react-icons/lib/fa/hand-o-up';
 import Fano from 'react-icons/lib/fa/hand-grab-o';
 
-//var URL = "http://webrates.truefx.com/rates/connect.html?f=csv";
 
  let offer_points_diff=[];
  let offer_figure_diff=[];
@@ -14,39 +12,14 @@ class App extends Component {
   //constructor for default states
         constructor(props){
         super(props);
-        //console.log(props)
         this.state={
         data:[]
         }
         this.loadCurrencyFromServer=this.loadCurrencyFromServer.bind(this);
         this.CheckPrevNext=this.CheckPrevNext.bind(this);       
         }
+  //Fetching data from the server
    loadCurrencyFromServer(){
-    // console.log(this.props.url);
- /*$.ajax({
-      url: "http://localhost:3001/currency_data",
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        this.setState({data: data});
-        console.log(this.state.data);
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-   */ 
-  /*
-  request("http://localhost:3001/currency_data", function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-console.log(response.body);
- this.setState({
-        data:response.body
-             })
-  }
-});
-*/
-
    fetch("http://localhost:3001/currency_data").then(function(response) {
     if (response.status >= 400) {
        throw new Error("Bad response from server");
@@ -54,65 +27,39 @@ console.log(response.body);
     return response.json();
   })
   .then(data => {               
-    //console.log(data);
-     //currency_info=data;
-     //console.log(currency_info);
-     //console.log(this.state.data)
-     
      this.setState({
         data:data 
              })
   });
-  //console.log(this.state.data);*/
   }
+  //Rendering of the component
   componentWillMount(props) {
-
+    //first call
      this.loadCurrencyFromServer();
-      this.interval = setInterval(this.loadCurrencyFromServer, 2000);
+     //call after every 20 sec(20000msec)
+      this.interval = setInterval(this.loadCurrencyFromServer, 20000);
   }
   
-  
+  //before rendering the updates Data check the diff
   shouldComponentUpdate(nextProps,nextState){
-        //console.log("Should I update?");
-        //console.log("PREV STATE",this.state.data);
-        
-        //console.log("NEXT STATE",nextState);
-
-        this.CheckPrevNext(this.state.data,nextState.data)
-        
+        this.CheckPrevNext(this.state.data,nextState.data)   
         return true;
-        //return nextProps.likes %2==0;
         }
        
+       //Checking the diff of prevState and NextState
         CheckPrevNext(prev_data,next_state){
-        // let prev_bid=[];
-         //let next_bid=[];
          offer_points_diff=[];
          offer_figure_diff=[];
-         /*
-         for(var index=0;index<prev_data.length;index++){
-          //console.log(prev_data[index],next_state[index]);
-         prev_bid[index]=prev_data[index]['Offer_big_figure'];
-          //console.log(prev_val['Offer_big_figure'])
-          //console.log("Diff:",next_val['Offer_big_figure']-prev_val['Offer_big_figure']);
-         }*/
          for(var index=0;index<next_state.length&&prev_data.length;index++){
-          //console.log(next_state[index]);
-          //next_bid[index]=next_state[index]['Offer_big_points'];
-           //prev_bid[index]=prev_data[index]['Offer_big_points'];
           offer_points_diff[index]= (next_state[index]['Offer_big_points'])-(prev_data[index]['Offer_big_points']);
           offer_figure_diff[index]= (next_state[index]['Offer_big_figure'])-(prev_data[index]['Offer_big_figure']);
-          //var prev_val=prev_data[index]
           console.log(offer_points_diff[index])
-          
          }
-         
-        // console.log("Diff:",next_bid[0]-prev_bid[0]);offer_figure_diff
+        
   }
-  
-
+//render the component data
   render(nextState) { 
-   
+    //Icon change
     const Icon_insert=(num)=>{
             if(parseInt(num,10)===0){
                 return(
@@ -131,7 +78,7 @@ console.log(response.body);
             }
 
           }
-        //console.log(currency_info);
+        //Data
         const ListItems =this.state.data.map((number,index) =>
         <tr key={number.Symbols}>
           <td>{number.Symbols}</td>
@@ -141,7 +88,6 @@ console.log(response.body);
           <td>{number.Offer_big_points}</td>
           <td>{Icon_insert(offer_points_diff[index])}</td>
         </tr>
-        
         );
       
       return (
@@ -161,7 +107,7 @@ console.log(response.body);
             <th>Change</th>
         </tr>
         {ListItems}
-      `</tbody>
+      </tbody>
         </table>
         </div>
       </div>
